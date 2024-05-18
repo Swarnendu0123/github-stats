367d9e7396bc09ed1c63fb7cd5f3e0815eff2e21
Swarnendu Bhandari <94161758+Swarnendu0123@users.noreply.github.com>
2024-05-18 14:25:42 +0530

    Added feature of getting all the PRs and Issues

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,33 @@ const cheerio = require('cheerio')
 
 export const getIssues = async (repoUrl: string, by?: string) => {
     try {
-        const response = await axios.get(`${repoUrl}/issues`);
+        const response = await axios.get(`${repoUrl}/issues?q=is%3Aissue+is%3Aall`);
         const $ = cheerio.load(response.data)
         const issues: Issue[] = []
         $('.js-issue-row').each((i: Number, elem: string) => {
-            const title = $(elem).find('.js-navigation-open').text().trim();
+            const title = $(elem).find('.markdown-title').text().trim();
             const url = "https://github.com" + $(elem).find('.js-navigation-open').attr('href');
-            const arr = $(elem).find('.opened-by').text().trim().split(' ')
-            const openedBy = arr[arr.length - 1]
+            // title include "Open issues created"
+            const openedBy = $(elem).find('.Link--muted').text().split("\n")[0]
 
             const issueLabel: string[] = []
             $(elem).find('.IssueLabel').each((i: Number, elem: string) => {
                 issueLabel.push($(elem).text().trim())
             })
+
+            let status = $(elem).find('.opened-by').text().trim()
+            if (status.includes('opened')) {
+                status = 'opened'
+            } else {
+                status = 'closed'
+            }
+
             issues.push({
                 title,
                 url,
                 openedBy,
                 issueLabel,
-                status: "open"
+                status
             });
         });
         if (by) {
@@ -37,25 +45,32 @@ export const getIssues = async (repoUrl: string, by?: string) => {
 
 export const getPulls = async (repoUrl: string, by?: string) => {
     try {
-        const response = await axios.get(`${repoUrl}/pulls`);
+        const response = await axios.get(`${repoUrl}/pulls?q=`);
         const $ = cheerio.load(response.data)
         const pulls: PullRequest[] = []
         $('.js-issue-row').each((i: Number, elem: string) => {
             const title = $(elem).find('.js-navigation-open').text().trim();
             const url = "https://github.com" + $(elem).find('.js-navigation-open').attr('href');
-            const arr = $(elem).find('.opened-by').text().trim().split(' ')
-            const openedBy = arr[arr.length - 1]
-
+            const openedBy = $(elem).find('.Link--muted').text()
             const issueLabel: string[] = []
             $(elem).find('.IssueLabel').each((i: Number, elem: string) => {
                 issueLabel.push($(elem).text().trim())
             })
+
+            let status = $(elem).find('.opened-by').text().trim()
+            if (status.includes('merged')) {
+                status = 'merged'
+            } else if (status.includes('closed')) {
+                status = 'closed'
+            } else {
+                status = 'open'
+            }
             pulls.push({
                 title,
                 url,
                 openedBy,
                 issueLabel,
-                status: "open"
+                status
             });
         });
         if (by) {
